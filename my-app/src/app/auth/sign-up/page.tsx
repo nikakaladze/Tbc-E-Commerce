a1cecd49b8edd63b2c0b36ee1673f47b5df4c283
nikakaladze <nikakaladze89@gmail.com>
2025-02-07 20:54:50 +0400

    chore:finish sign-up functionality

--- my-app/src/app/auth/sign-up/page.tsx
+++ my-app/src/app/auth/sign-up/page.tsx
@@ -1,42 +1,41 @@
-import { getCurrentSession, loginUser, registerUser } from '@/actions/auth';
-import SignUp from '@/components/auth/SignUp';
-import { redirect } from 'next/navigation';
-import React from 'react'
-import zod from 'zod' ;
-const signUpSchema = zod.object({
-  email:zod.string().email(),
-  password:zod.string().min(5)
-})
+import { getCurrentSession, loginUser, registerUser } from "@/actions/auth";
+import SignUp from "@/components/auth/SignUp";
+import { redirect } from "next/navigation";
+import React from "react";
+import zod from "zod";
+
+const SignUpSchema = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(5),
+});
 
 const SignUpPage = async () => {
-    const {user} = await getCurrentSession();
-    if(user){
-        return redirect("/")
-    }
-    const action = async (prevState:any, formData:FormData) =>{
-        "use server"
-        const parsed = signUpSchema.safeParse(Object.fromEntries(formData));
-        if(!parsed.success){
-            return{
+  const { user } = await getCurrentSession();
 
-              message:'invalid Form Data',
-            }
-              
-            
-        }
-        const { email, passwrod } = parsed.data;
-        const {user,error } = await registerUser(email,passwrod)
-        if(error){
-          return{message:error}
-        }else if(user){
-          await loginUser(email,passwrod)
-          return  redirect("/")
-        }
-       
+  if (user) {
+    return redirect("/");
+  }
+
+  const action = async (prevState: any, formData: FormData) => {
+    "use server";
+    const parsed = SignUpSchema.safeParse(Object.fromEntries(formData));
+    if (!parsed.success) {
+      return {
+        message: "Invalid form data",
+      };
     }
-  return (
-   <SignUp action={action}/>
-  )
-}
+
+    const { email, password } = parsed.data;
+    const { user, error } = await registerUser(email, password);
+    if (error) {
+      return { message: error };
+    } else if (user) {
+      await loginUser(email, password);
+      return redirect("/");
+    }
+  };
+
+  return <SignUp action={action} />;
+};
 
 export default SignUpPage;
