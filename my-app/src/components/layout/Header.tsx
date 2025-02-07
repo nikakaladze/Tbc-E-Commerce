1e87bd6981518316fa76d85a0c6cdd0640a683d0
nikakaladze <nikakaladze89@gmail.com>
2025-02-07 17:03:55 +0400

    chore:working at header

---
+++ my-app/src/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+const AnnouncementBar = () => {
+    return(
+
+        <div className='w-full bg-black py-2'>
+            <div className='container'>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
