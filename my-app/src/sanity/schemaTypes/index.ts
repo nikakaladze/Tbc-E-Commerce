7acf48933d4d38fe288ec94d4ba738e6cc482fa5
nikakaladze <nikakaladze89@gmail.com>
2025-02-07 22:55:43 +0400

    chore:working to implements sanity

--- my-app/src/sanity/schemaTypes/index.ts
+++ my-app/src/sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
-import { order, orderItem, shippingAddress } from '@/sanity/schemaTypes/schemas/order'
-import { product } from '@/sanity/schemaTypes/schemas/product'
-import { productCategory } from '@/sanity/schemaTypes/schemas/product-category'
-import { promotionCampaign } from '@/sanity/schemaTypes/schemas/promotion-campaign'
-import { promotionCode } from '@/sanity/schemaTypes/schemas/promotion-codes'
+import { order, orderItem, shippingAddress } from '../schemaTypes/schemas/order'
+import {product } from './schemas/products'
+import { productCategory } from '../schemaTypes/schemas/product-category'
+import { promotionCampaign } from '../schemaTypes/schemas/promotion-campaign'
+import { promotionCode } from '../schemaTypes/schemas/promotion-codes'
 import { type SchemaTypeDefinition } from 'sanity'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
